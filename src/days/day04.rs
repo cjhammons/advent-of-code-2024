2e25a3a318724cb9badaf1fd0d59dea20396ff02
Curtis Hammons <chammons@boingo.com>
2024-12-04 09:04:43 -0800

    stubbed out day4

---
+++ src/days/day04.rs
@@ -0,0 +1,30 @@
+pub fn run() {
+    println!("Day 04");
+    println!("Part 1: {}", part1("src/inputs/day04.txt".to_string()));
+    println!("Part 2: {}", part2("src/inputs/day04.txt".to_string()));
+}
+
+fn part1(file_name: String) -> i32 {
+    return 0
+}
+
+fn part2(file_name: String) -> i32 {
+    return 0
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part1_with_example() {
+        let solution = part1("src/inputs/day04_test.txt".to_string());
+        assert_eq!(solution, 18);
+    }
+
+    #[test]
+    fn test_part2_with_example() {
+        let solution = part2("src/inputs/day04_test.txt".to_string());
+        assert_eq!(solution, -1);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -16,6 +16,7 @@ fn main() {
         1 => days::day01::run(),
         2 => days::day02::run(),
         3 => days::day03::run(),
+        4 => days::day04::run(),
         _ => println!("Day {} not implemented", args.day)
     };
 }
